449ebf171a3687796848e35f8d6dac83083a3849
markdave123-py <nwaekwudavid@gmail.com>
2024-07-20 23:35:33 +0100

    changes

--- src/core/config/database.js
+++ src/core/config/database.js
@@ -5,15 +5,38 @@ import { config } from "./env.js";
 const { host, name, password, user } = config.db;
 
 
-export const sequelizeConn = new Sequelize(name, user, password, {
-  host: host,
-  dialect: "postgres",
-  logging: false,
-  omitNull: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+// export const sequelizeConn = new Sequelize(name, user, password, {
+//   host: host,
+//   dialect: "postgres",
+//   logging: false,
+//   omitNull: false,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
+
+
+export const sequelizeConn = new Sequelize(
+  "postgresql://ridepal_user:vLBOPCzAiM5T6egUCv5UEXoEcEYl8CNM@dpg-cqe0hspu0jms7392pd50-a.oregon-postgres.render.com/ridepal",
+  {
+    // host: host,
+    dialect: "postgres",
+    dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For self-signed certificates
+    },
   },
-});
+    logging: false,
+    omitNull: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
