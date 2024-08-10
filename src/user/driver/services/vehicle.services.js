b6efeb5098a6f7434b2b952d0041def0c425d52d
markdave123-py <nwaekwudavid@gmail.com>
2024-08-10 12:56:34 +0100

    changes

--- src/user/driver/services/vehicle.services.js
+++ src/user/driver/services/vehicle.services.js
@@ -1,22 +1,34 @@
 import Vehicle from "../../model/vehicle.js";
 
-
 export class VehicleService {
+  static async createVehicle(vehicleData) {
+    return await Vehicle.create(vehicleData);
+  }
 
-    static async createVehicle(vehicleData) {
-        return await Vehicle.create(vehicleData);
-    }
+  static async getVehicleById(vehicleId) {
+    const vehicle = await Vehicle.findByPk(vehicleId);
+    if (!vehicle) return null;
+    return {
+      plateNumber: vehicle.plateNumber,
+      seatNumber: vehicle.seatNumber,
+      color: vehicle.color,
+      model: vehicle.model,
+      ownerId: vehicle.ownerId,
+    };
+  }
 
-    static async getVehicleById(vehicleId) {
-        const vehicle = await Vehicle.findByPk(vehicleId);
-        if (!vehicle) return null
-        return {
-            plateNumber: vehicle.plateNumber,
-            seatNumber: vehicle.seatNumber,
-            color: vehicle.color,
-            model: vehicle.model,
-            ownerId: vehicle.ownerId
-        }
-    }
+  static async getVehicleByOwnerId(ownerId) {
+    const vehicle = await Vehicle.findOne({
+      where: { ownerId },
+    });
 
+    if (!vehicle) return null;
+    return {
+      id: vehicle.id,
+      plateNumber: vehicle.plateNumber,
+      seatNumber: vehicle.seatNumber,
+      color: vehicle.color,
+      model: vehicle.model,
+    };
+  }
 }
